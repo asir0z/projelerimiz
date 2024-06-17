155258edfb3bd950ad26252f0dea96d11ec3a8df
Asır Öz <asir01oz@gmail.com>
2024-06-17 06:26:57 +0300

    degisiklikler

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//template engine
+app.set("view engine", "ejs");
+
+//middlewares
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.send("INDEX");
 });
